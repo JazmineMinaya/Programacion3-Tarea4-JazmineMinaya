dc493443824b7db7d6e9d20fe90496f242d2a586
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-14 09:42:47 -0400

    Métodos para validarEdad y solicitarEdad correspondientes al ejercicio 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+    }
+
+    public static void validarEdad(int edad) throws EdadInvalidaException {
+        if (edad < 18) {
+            throw new EdadInvalidaException("Debe ser mayor de edad.");
+        }
+    }
+
+    public static void solicitarEdad() {
+        System.out.println("\nVALIDADOR DE EDAD:");
+
+        try {
+            System.out.println("\nIngrese su edad:");
+            int edad = scanner.nextInt();
+            scanner.nextLine();
+
+            validarEdad(edad);
+            System.out.println("\nEdad ingresada valida.");
+        }
+        catch(EdadInvalidaException e) {
+            System.out.println("\nError: " + e.getMessage());
+        }
     }
 }
